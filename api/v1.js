48a3af522895475342738e181dbec3d7dae3d9ba
r58Playz <toshit307@gmail.com>
2022-08-28 01:19:03 +0000

    fix

--- api/v1.js
+++ api/v1.js
@@ -28,19 +28,4 @@ const handler = async (req, res) => {
   }
 };
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, Access-Control-Allow-Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-}
-
-module.exports = allowCors(handler);
+export default handler;
