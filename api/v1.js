d250a8b599d5497868df8e9bdcced43b9e7240c4
r58Playz <toshit307@gmail.com>
2022-08-28 03:21:25 +0000

    adfasgafdsvsdadf

--- api/v1.js
+++ api/v1.js
@@ -11,7 +11,7 @@ const handler = async (req, res) => {
 
   try {
     //console.log("req.headers " + req.headers["Authorization"]);
-    console.log("req.getHeaders() " + req.getHeaders().authorization);
+    console.log("req.getHeaders() " + req.headers.authorization);
     const response = await fetch(url, {
       headers: {
         Accept: 'application/json',
